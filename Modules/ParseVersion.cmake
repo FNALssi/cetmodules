b9feef07d8344d4b83252ce19d3ecf27d40c5370
Patrick Gartung <gartung@fnal.gov>
2019-04-24 17:33:00 -0500

    Change _ups_ -> _underscored_

--- Modules/ParseVersion.cmake
+++ Modules/ParseVersion.cmake
@@ -1,7 +1,7 @@
 # parse an underscored version string and set the cmake project versions
 #
 # set_dot_version ( PRODUCTNAME _VERSION )
-# set_version_from_ups( _VERSION )
+# set_version_from_underscored( _VERSION )
 # parse_underscored_version( _VERSION )
 #  PRODUCTNAME - product name
 #  _VERSION - ups version of the form vx_y_z
@@ -10,7 +10,7 @@ macro( parse_underscored_version _VERSION )
 
   STRING(REGEX MATCHALL "_" ulist ${_VERSION} ) 
   list( LENGTH ulist nunder )
-  ##message(STATUS "parse_ups_version: ${_VERSION} has ${nunder} underscores" )
+  ##message(STATUS "parse_underscored_version: ${_VERSION} has ${nunder} underscores" )
   if ( ${nunder} STREQUAL 0 )
     STRING( REGEX REPLACE "^[v](.*)$" "\\1" VMAJ "${_VERSION}" )
   elseif( ${nunder} STREQUAL 1 )
@@ -32,21 +32,21 @@ macro( parse_underscored_version _VERSION )
     STRING( REGEX REPLACE "^[v](.*)[_](.*)[_](.*)[_](.*)$" "\\3" VPRJ "${_VERSION}" )
     STRING( REGEX REPLACE "^[v](.*)[_](.*)[_](.*)[_](.*)$" "\\4" VPT "${_VERSION}" )
   endif()
-  ##message(STATUS "parse_ups_version: version parses to ${VMAJ}.${VMIN}.${VPRJ}.${VPT}" )
+  ##message(STATUS "parse_underscored_version: version parses to ${VMAJ}.${VMIN}.${VPRJ}.${VPT}" )
 
-endmacro( parse_ups_version )
+endmacro( parse_underscored_version )
 
-macro( set_version_from_ups _VERSION )
+macro( set_version_from_underscored _VERSION )
 
-  parse_ups_version( ${_VERSION} )
+  parse_underscored_version( ${_VERSION} )
 
   set( VERSION_MAJOR ${VMAJ} CACHE STRING "Package major version" FORCE)
   set( VERSION_MINOR ${VMIN} CACHE STRING "Package minor version" FORCE )
   set( VERSION_PATCH ${VPRJ} CACHE STRING "Package patch version" FORCE )
   set( VERSION_TWEAK ${VPT} CACHE STRING "Package tweak version" FORCE )
-  ##message(STATUS "set_version_from_ups: project version is ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}.${VERSION_TWEAK}" )
+  ##message(STATUS "set_version_from_underscored: project version is ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}.${VERSION_TWEAK}" )
 
-endmacro( set_version_from_ups )
+endmacro( set_version_from_underscored )
 
 macro( set_dot_version PRODUCTNAME _VERSION )
 
