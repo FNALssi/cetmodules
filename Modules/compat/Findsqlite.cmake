7ff28af638362e803e7e04e6bd0b2f71c153140f
Chris Green <greenc@fnal.gov>
2020-11-13 21:22:02 -0600

    Ensure we pick up the appropriate SQLite library regardless of how we ask for it.

--- Modules/compat/Findsqlite.cmake
+++ Modules/compat/Findsqlite.cmake
@@ -15,7 +15,11 @@ endif()
 # In B4 the official search to find our special library first. We do NOT
 # want to use NAMES_PER_DIR here.
 find_library(SQLite3_LIBRARY NAMES sqlite3_ups sqlite3 sqlite)
+set(_cet_sqlite_cmake_module_path "${CMAKE_MODULE_PATH}")
+set(CMAKE_MODULE_PATH) # Don't want to find ourselves and loop.
 find_package(SQLite3)
+set(CMAKE_MODULE_PATH "${_cet_sqlite_cmake_module_path}")
+unset(_cet_sqlite_cmake_module_path)
 
 # Set a variable for backward compatibility.
 if (SQLite3_FOUND AND ${PROJECT_NAME}_OLD_STYLE_CONFIG_VARS)
@@ -30,7 +34,7 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLite3 NAME_MISMATCHED
-  REQUIRED_VARS SQLite3_INCLUDE_DIRS SQLite3_LIBRARIES SQLITE3)
+  REQUIRED_VARS SQLite3_INCLUDE_DIRS SQLite3_LIBRARIES)
 if (_cet_sqlite_pkg_prefix)
   foreach (_cet_sqlite_var IN LISTS _cet_sqlite_sqlite_var_names)
     set(${_cet_sqlite_pkg_prefix}_${_cet_sqlite_var} ${SQLite3_${_cet_sqlite_var}})
