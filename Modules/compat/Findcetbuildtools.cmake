aa73cd9463c07c22ab01079e04b7451a7b5eab8c
Chris Green <greenc@fnal.gov>
2020-11-14 22:41:24 -0600

    Deactivate deprecation warnings for cetbuildtools-using packages.

--- Modules/compat/Findcetbuildtools.cmake
+++ Modules/compat/Findcetbuildtools.cmake
@@ -1,6 +1,8 @@
 set(cetbuildtools_VERSION 7.17.01)
 set(cetbuildtools_UPS_VERSION v7_17_01)
 
+set(CET_WARN_DEPRECATED) # Quiet warnings for known old package.
+
 find_package(cetmodules NO_MODULE REQUIRED)
 
 set(cetbuildtools_BINDIR ${cetmodules_BINARY_DIR})
