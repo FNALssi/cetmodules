150067a4ed11c67d7af99bcdc4f0c770b908a825
Chris Green <greenc@fnal.gov>
2020-11-21 00:03:13 -0600

    Fix issues with backward-compatibility variables.

--- Modules/compat/FindUpsRoot.cmake
+++ Modules/compat/FindUpsRoot.cmake
@@ -19,25 +19,10 @@ macro(find_ups_root)
   cet_find_package(${_FUP_PROJECT} ${_FUP_DOT_VERSION} ${_FUP_UNPARSED_ARGUMENTS})
 
   # Convenience variables for backward compatibility.
-  if (${_FUP_PROJECT}_FOUND AND NOT _FUP_INTERFACE)
+  if (${_FUP_PROJECT}_FOUND AND NOT (_FUP_INTERFACE OR
+        _FUP_INCLUDED_${_FUP_PROJECT}))
     set(ROOTSYS $ENV{ROOTSYS})
-    get_property(_fur_cache_vars DIRECTORY PROPERTY CACHE_VARIABLES)
-    list(FILTER _fur_cache_vars INCLUDE REGEX "^ROOT_(.*)_LIBRARY$")
-    list(TRANSFORM _fur_cache_vars REPLACE "^ROOT_(.*)_LIBRARY$" "\\1"
-      OUTPUT_VARIABLE _fur_old_varnames)
-    if (${_FUP_PROJECT}_VERSION VERSION_GREATER_EQUAL 6.10.04) # Use targets
-      list(TRANSFORM _fur_old_varnames PREPEND ${_FUP_PROJECT}::
-        OUTPUT_VARIABLE _fur_targets) # Use targets.
-      set(val_list _fur_targets)
-      set(val_string "\${_fur_var}")
-    else() # Use cache variables instead
-      set(val_list _fur_cache_vars)
-      set(val_string "\$CACHE{\${_fur_var}}")
-    endif()
-    list(TRANSFORM _fur_old_varnames TOUPPER)
-    foreach (_fur_old_varname _fur_var IN ZIP_LISTS _fur_old_varnames ${val_list})
-      cmake_language(EVAL CODE "set(${_fur_old_varname} ${val_string})")
-    endforeach()
+    _set_root_lib_vars()
     set(ROOT_GENREFLEX "$CACHE{ROOT_genreflex_CMD}")
     set(ROOTCLING "$CACHE{ROOT_rootcling_CMD}")
     set(ROOT_BASIC_LIB_LIST ${ROOT_CORE} ${ROOT_CINT} ${ROOT_RIO}
@@ -47,10 +32,29 @@ macro(find_ups_root)
     set(ROOT_GUI_LIB_LIST ${ROOT_GUI} ${ROOT_BASIC_LIB_LIST})
     set(ROOT_EVE_LIB_LIST ${ROOT_EVE} ${ROOT_EG} ${ROOT_TREEPLAYER}
       ${ROOT_GEOM} ${ROOT_GED} ${ROOT_RGL} ${ROOT_GUI_LIB_LIST})
+    if (NOT _FUP_INCLUDED_${FUP_PROJECT})
+      include_directories($ENV{ROOT_INC})
+      set(_FUP_INCLUDED_${FUP_PROJECT} TRUE)
+    endif()
   endif()
-
-  # Backward compatibility only.
-  include_directories($ENV{ROOT_INC})
 endmacro()
+
+function(_set_root_lib_vars)
+  file(GLOB libs LIST_DIRECTORIES FALSE
+    ${ROOTSYS}/lib/lib*.so ${ROOTSYS}/lib/lib*.so)
+  list(FILTER libs EXCLUDE REGEX "Dict\\.(so|a)$")
+  list(TRANSFORM libs REPLACE "^.*/lib(.*)\.so$" "\\1" OUTPUT_VARIABLE libnames)
+  list(TRANSFORM libnames TOUPPER OUTPUT_VARIABLE old_varnames)
+  if (${_FUP_PROJECT}_VERSION VERSION_GREATER_EQUAL 6.10.04) # Use targets
+    list(TRANSFORM libnames PREPEND ${_FUP_PROJECT}::
+      OUTPUT_VARIABLE targets) # Use targets.
+    set(val_list targets)
+  else() # Use full library locations.
+    set(val_list libs)
+  endif()
+  foreach (old_varname var IN ZIP_LISTS old_varnames ${val_list})
+    set(ROOT_${old_varname} "${var}" PARENT_SCOPE)
+  endforeach()
+endfunction()
 
 cmake_policy(POP)
