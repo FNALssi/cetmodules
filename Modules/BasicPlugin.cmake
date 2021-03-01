2cf72108a7643eac2abf9a66acd653a505af7f46
Chris Green <greenc@fnal.gov>
2021-03-01 17:41:59 -0600

    Significant improvements to clarity, usability and correctness of basic_plugin().
    * Improve documentation and explanations therein.
    
    * Option name changes: PLUGIN -> REG
    
    * Be more careful about the use of "base" as a variable name component.
    
    * New dependency label "CONDITIONAL" to resolve problems faced by
      intermediate functions needing to add dependencies without assuming a
      single/double library setup. Also helps plugin authors whose
      implementation is entirely inline.
    
    * Clarify and improve the distinction between the implementation and
      registration libraries (where separated), and the relationships
      between dependency labels (PUBLIC, PRIVATE, etc.) and library
      configuration.

--- Modules/BasicPlugin.cmake
+++ Modules/BasicPlugin.cmake
@@ -14,27 +14,83 @@ cmake_minimum_required(VERSION 3.18.2 FATAL_ERROR)
 
 include(CetPackagePath)
 include(CetProcessLiblist)
+include(CetRegexEscape)
 
-set(cet_bp_flags ALLOW_UNDERSCORES BASENAME_ONLY NOP NO_EXPORT NO_INSTALL USE_BOOST_UNIT USE_PRODUCT_NAME VERSION)
+set(cet_bp_flags ALLOW_UNDERSCORES BASENAME_ONLY NOP NO_EXPORT NO_INSTALL
+  USE_BOOST_UNIT USE_PRODUCT_NAME VERSION)
 set(cet_bp_one_arg_opts EXPORT_SET SOVERSION)
-set(cet_bp_list_options ALIAS IMPL_SOURCE LIBRARIES LOCAL_INCLUDE_DIRS PLUGIN_SOURCE SOURCE)
+set(cet_bp_list_options ALIAS IMPL_SOURCE LIBRARIES LOCAL_INCLUDE_DIRS
+  REG_SOURCE SOURCE)
+
+cet_regex_escape(${cet_bp_flags} ${cet_bp_one_arg_opts} ${cet_bp_list_options} VAR _e_bp_args)
+string(REPLACE ";" "|" _e_bp_args "${_e_bp_args}")
 
 #[================================================================[.rst:
 .. cmake:command:: basic_plugin
 
-   Create a plugin module.
+   Create a plugin library, with or without a separate registration
+   library to avoid One Definition Rule (ODR) violations.
 
    **Synopsis:**
      .. code-block:: cmake
 
-        basic_plugin(<name> <type> [<options>])
+        basic_plugin(<name> <suffix> [<options>])
+
+   **Source specification options:**
+
+     ``IMPL_SOURCE <implementation-source>...`` Specify source to
+       compile into the plugin's interface implementation library, if
+       appropriate. The implementation should *not* invoke any
+       registration definition macros or the ODR will be violated.
+
+     ``REG_SOURCE <registration-source>...``
+       Specify source to compile into the plugin registration library,
+       intended only for runtime injection (via *e.g.* ``dlopen()``)
+       into an executable, and not for dynamic linking.
+
+     .. note::
+
+        * If ``REG_SOURCE`` is omitted, we look for ``<name>_<suffix>.cc``
+
+        * If ``IMPL_SOURCE`` is omitted, we look for ``<name>.cc``
+
+   **Dependency specification options:**
+
+     ``LIBRARIES [INTERFACE|PRIVATE|PROTECTED|PUBLIC|REG] <library-dependency>...``
+
+       Specify targets and/or libraries upon which the implementation
+       (``INTERFACE``, ``PUBLIC``, ``PRIVATE``), or registration
+       (``REG``) libraries should depend. If implementation and
+       registration share a dependency not inherited by public callers
+       of the implementation, specify the library as both ``PRIVATE``
+       and ``REG``.
+
+     .. note::
+
+        * The registration library has an automatic dependence on the
+          implementation library (if present).
+
+        * If ``PUBLIC`` or ``INTERFACE`` dependencies are specified and
+          there is no implementation source, then the library will be
+          built shared rather than as a module, and all responsibility
+          for ODR violations rests with the library builder.
+
+        * An additional dependency designation, ``CONDITIONAL``, is
+          accepted and is intended for use by intermediate functions
+          adding dependencies to a library. ``CONDITIONAL`` is identical
+          to ``PUBLIC`` without making a statement about the shared or
+          module nature of the combined implementation/registration
+          library or the presence of a public (non-plugin) calling
+          interface.
+
+   **Other options:**
 
      ``ALIAS <alias>...``
        Create the specified CMake alias targets to the implementation
        library.
 
      ``ALLOW_UNDERSCORES``
-       Normally, neither ``<name>`` nor ``<type>`` may contain
+       Normally, neither ``<name>`` nor ``<suffix>`` may contain
        underscores in order to avoid possible ambiguities. Allow them
        with this option at your own risk.
 
@@ -45,41 +101,16 @@ set(cet_bp_list_options ALIAS IMPL_SOURCE LIBRARIES LOCAL_INCLUDE_DIRS PLUGIN_SO
      ``EXPORT_SET <export-name>``
        Add the library to the ``<export-name>`` export set.
 
-     ``IMPL_SOURCE <impl-source>``
-       Specify sources to compile into the plugin's interface
-       implementation library, if it has an interface header. These
-       should be separated from the plugin registration sources to avoid
-       violating the one definition rule.
-
-       .. seealso:: ``PLUGIN_SOURCE``
-
-     ``LIBRARIES <library-dependency>...``
-       Dependencies against which to link.
-
-       .. note:: In addition to the usual signifiers of ``INTERFACE``,
-       ``PRIVATE``, and ``PUBLIC``, this option understands ``PLUGIN` to
-       denote specifically dependencies of the library containing the
-       plugin registration code (as distinct from the interface
-       implementation library, if it exists).
-
      ``LOCAL_INCLUDE_DIRS <dir>...``
        Headers may be found in ``<dir>``... at build time.
 
      ``NOP``
        Option / argument disambiguator; no other function.
 
+     ``NO_EXPORT``
+
      ``NO_INSTALL``
-       Do not install the generated plugin.
-
-     ``PLUGIN_SOURCE <plugin-source>``
-       Specify sources to compile into the plugin registration library;
-       defaults to ``<name>_<type>.cc``.
-
-     ``SOURCE <source>...``
-       Specify sources to compile into the plugin.
-
-       .. deprecated:: 2.10
-          use ``IMPL_SOURCE``, ``PLUGIN_SOURCE`` and ``LIBRARIES PLUGIN`` instead.
+       Do not install the generated library or libraries.
 
      ``SOVERSION <version>``
        The library's compatibility version (*cf*
@@ -89,11 +120,6 @@ set(cet_bp_list_options ALIAS IMPL_SOURCE LIBRARIES LOCAL_INCLUDE_DIRS PLUGIN_SO
        The plugin uses Boost unit test functions and should be compiled
        and linked accordingly.
 
-     ``USE_PRODUCT_NAME``
-
-       .. deprecated:: 2.0
-          use ``USE_PACKAGE_NAME`` instead.
-
      ``USE_PACKAGE_NAME``
        The package name will be prepended to the plugin library name,
        separated by ``_``
@@ -102,24 +128,35 @@ set(cet_bp_list_options ALIAS IMPL_SOURCE LIBRARIES LOCAL_INCLUDE_DIRS PLUGIN_SO
        The library's build version will be set to
        :cmake:variable:`PROJECT_NAME` (*cf* :cmake:prop_tgt:`VERSION`).
 
+   **Deprecated options:**
+
+     ``SOURCE <source>...``
+       Specify sources to compile into the plugin.
+
+      .. deprecated:: 2.11
+         use ``IMPL_SOURCE``, ``REG_SOURCE`` and ``LIBRARIES REG``
+         instead.
+
+     ``USE_PRODUCT_NAME``
+
+       .. deprecated:: 2.0
+          use ``USE_PACKAGE_NAME`` instead.
+
    **Non-option arguments:**
 
      ``<name>``
 
      The name stem for the library to be generated.
 
-     ``<type>``
+     ``<suffix>``
 
-     The type of plugin to be generated.
-
-   .. note:: The plugin generated will be named ``<prefix><name>_<type><suffix>``.
+     The category of plugin to be generated.
 
    .. seealso:: :cmake:command:`cet_make_library`
 #]================================================================]
 function(basic_plugin NAME SUFFIX)
   cmake_parse_arguments(PARSE_ARGV 2 BP
     "${cet_bp_flags}" "${cet_bp_one_arg_opts}" "${cet_bp_list_options}")
-  set(thunk)
   if (BP_UNPARSED_ARGUMENTS)
     warn_deprecated("unprocessed arguments" NEW "LIBRARIES")
     list(APPEND BP_LIBRARIES ${BP_UNPARSED_ARGUMENTS})
@@ -128,7 +165,7 @@ function(basic_plugin NAME SUFFIX)
     message(FATAL_ERROR "BASENAME_ONLY AND USE_PRODUCT_NAME are mutually exclusive")
   endif()
   if (BP_BASENAME_ONLY)
-    set(plugin_base "${NAME}")
+    set(plugin_stem "${NAME}")
   else()
     cet_package_path(CURRENT_SUBDIR)
     if (NOT BP_ALLOW_UNDERSCORES)
@@ -139,26 +176,20 @@ function(basic_plugin NAME SUFFIX)
         message(FATAL_ERROR  "found underscore in plugin name: ${NAME}" )
       endif()
     endif()
-    string(REPLACE "/" "_" plugin_base "${CURRENT_SUBDIR}")
+    string(REPLACE "/" "_" plugin_stem "${CURRENT_SUBDIR}")
     if (BP_USE_PRODUCT_NAME)
-      string(JOIN "_" plugin_base "${PROJECT_NAME}" "${plugin_base}")
+      string(JOIN "_" plugin_stem "${PROJECT_NAME}" "${plugin_stem}")
     endif()
-    string(JOIN "_" plugin_base  "${plugin_base}" "${NAME}")
+    string(JOIN "_" plugin_stem  "${plugin_stem}" "${NAME}")
   endif()
   if (BP_SOURCE)
-    warn_deprecated("SOURCE" NEW "IMPL_SOURCE, PLUGIN_SOURCE and LIBRARIES PLUGIN")
-    if (BP_PLUGIN_SOURCE)
-      message(FATAL_ERROR "SOURCE and PLUGIN_SOURCE are mutually exclusive")
+    warn_deprecated("SOURCE" NEW "IMPL_SOURCE, REG_SOURCE and LIBRARIES REG")
+    if (BP_REG_SOURCE)
+      message(FATAL_ERROR "SOURCE and REG_SOURCE are mutually exclusive")
     endif()
-    set(BP_PLUGIN_SOURCE "${BP_SOURCE}")
-  elseif (NOT BP_PLUGIN_SOURCE)
-    set(BP_PLUGIN_SOURCE "${NAME}_${SUFFIX}.cc")
-  endif()
-  set(shared_ok)
-  if (BP_IMPL_SOURCE STREQUAL BP_PLUGIN_SOURCE)
-    # Caller knows what they're doing.
-    unset(BP_IMPL_SOURCE)
-    set(shared_ok TRUE)
+    set(BP_REG_SOURCE "${BP_SOURCE}")
+  elseif (NOT BP_REG_SOURCE)
+    set(BP_REG_SOURCE "${NAME}_${SUFFIX}.cc")
   endif()
   set(cml_args)
   ##################
@@ -178,8 +209,9 @@ function(basic_plugin NAME SUFFIX)
     cet_passthrough(FLAG APPEND BP_${kw} cml_impl_args)
   endforeach()
   ##################
-  set(thunk)
-  cmake_parse_arguments(BPL "" "" "INTERFACE;PRIVATE;PUBLIC;PLUGIN" ${BP_LIBRARIES})
+  set(target_thunk)
+  cmake_parse_arguments(BPL "" ""
+    "CONDITIONAL;INTERFACE;PRIVATE;PUBLIC;REG" ${BP_LIBRARIES})
   list(APPEND BPL_PUBLIC ${BPL_UNPARSED_ARGUMENTS})
   if (NOT BP_IMPL_SOURCE) # See if we can find one.
     get_filename_component(if_plugin_impl "${NAME}.cc" REALPATH)
@@ -187,67 +219,70 @@ function(basic_plugin NAME SUFFIX)
       set(BP_IMPL_SOURCE "${NAME}.cc")
     endif()
   endif()
-  foreach (kw IN ITEMS INTERFACE PUBLIC PRIVATE)
-    cet_passthrough(IN_PLACE BPL_${kw})
-  endforeach()
-  if (BP_IMPL_SOURCE OR NOT BPL_PUBLIC)
-    set(LIB_TYPE MODULE)
+  if (BP_IMPL_SOURCE OR NOT (BPL_INTERFACE OR BPL_PUBLIC))
+    set(REG_LIB_TYPE MODULE)
     if (BP_IMPL_SOURCE)
-      list(PREPEND BPL_PLUGIN "${plugin_base}_${SUFFIX}")
-      cet_make_library(LIBRARY_NAME "${plugin_base}_${SUFFIX}"
+      list(APPEND BPL_PUBLIC ${BPL_CONDITIONAL})
+      list(REMOVE_DUPLICATES BPL_PUBLIC)
+      cet_make_library(LIBRARY_NAME "${plugin_stem}_${SUFFIX}"
         SOURCE ${BP_IMPL_SOURCE}
-        LIBRARIES ${BPL_INTERFACE} ${BPL_PUBLIC} ${BPL_PRIVATE} NOP
-        ${cml_common_args} ${cml_impl_args}
-        )
+        LIBRARIES
+        INTERFACE ${BPL_INTERFACE}
+        PUBLIC ${BPL_PUBLIC}
+        PRIVATE ${BPL_PRIVATE}
+        NOP ${cml_common_args} ${cml_impl_args})
       # For backward compatibility purposes, we retain the vanilla
       # target name but have a different name for the implementation
       # library on disk.
-      set_target_properties("${plugin_base}_${SUFFIX}"
-        PROPERTIES OUTPUT_NAME "${plugin_base}_${SUFFIX}_impl"
+      set_target_properties("${plugin_stem}_${SUFFIX}"
+        PROPERTIES OUTPUT_NAME "${plugin_stem}"
       )
       # Thunk the target name of the plugin library so we don't attempt
       # to link to it, but retain the vanilla library name for backward
       # compatibility.
-      set(thunk _plugin)
-      # Trim the plugin's library list.
-      set(BP_LIBRARIES PRIVATE "${plugin_base}_${SUFFIX}" ${BPL_PLUGIN})
+      set(target_thunk _plugin)
+      # Trim the library list for the registration library:
+      set(BP_LIBRARIES PRIVATE "${plugin_stem}_${SUFFIX}" ${BPL_REG})
       unset(cml_impl_args)
-    else()
-      set(BP_LIBRARIES ${BPL_PRIVATE} PRIVATE ${BPL_PLUGIN})
-      list(REMOVE_DUPLICATES BP_LIBRARIES)
-      list(PREPEND BP_LIBRARIES ${BPL_INTERFACE} ${BPL_PUBLIC})
+    else() # One combined module that we've been told is safe:
+      set(BP_LIBRARIES PRIVATE ${BPL_CONDITIONAL} ${BPL_PRIVATE} ${BPL_REG})
     endif()
   else()
-    set(LIB_TYPE SHARED)
-    set(BP_LIBRARIES ${BPL_INTERFACE} ${BPL_PUBLIC} ${BPL_PRIVATE} PRIVATE ${BPL_PLUGIN})
-    if (NOT shared_ok AND CET_WARN_DEPRECATED)
-      message(AUTHOR_WARNING "prefer separate compilation units for implementation (IMPL_SOURCE) and plugin registration macros (PLUGIN_SOURCE, LIBRARIES PLUGIN)")
+    set(REG_LIB_TYPE SHARED)
+    list(APPEND BPL_PUBLIC ${BPL_CONDITIONAL})
+    list(REMOVE_DUPLICATES BPL_PUBLIC)
+    list(APPEND BPL_PRIVATE ${BPL_REG})
+    list(REMOVE_DUPLICATES BPL_PRIVATE)
+    set(BP_LIBRARIES INTERFACE ${BPL_INTERFACE}
+      PUBLIC ${BPL_PUBLIC} PRIVATE ${BPL_PRIVATE})
+    if (CET_WARN_DEPRECATED)
+      message(AUTHOR_WARNING "prefer separate compilation units for implementation (IMPL_SOURCE) and plugin registration macros (REG_SOURCE, LIBRARIES REG) due to possible consequences of One Definition Rule violation")
     endif()
   endif()
   ##################
   # These items are applicable only to the implementation library.
-  cet_passthrough(IN_PLACE BP_PLUGIN_SOURCE
+  cet_passthrough(IN_PLACE BP_REG_SOURCE
     KEYWORD SOURCE EMPTY_KEYWORD NO_SOURCE)
   ##################
   # Make the plugin library, to which we should not normally link
-  # directly (see PLUGIN_SOURCE, above).
+  # directly (see REG_SOURCE, above).
   #
   # Module-type libraries containing only plugin registration code can
   # be stripped.
-  cet_passthrough(FLAG APPEND thunk KEYWORD STRIP_LIBS cml_impl_args)
-  if (LIB_TYPE STREQUAL "MODULE" AND NOT NO_INSTALL)
+  cet_passthrough(FLAG APPEND target_thunk KEYWORD STRIP_LIBS cml_impl_args)
+  if (REG_LIB_TYPE STREQUAL "MODULE" AND NOT NO_INSTALL)
     # We don't want the plugin-only library visible as an exported target.
     list(APPEND cml_impl_args NO_EXPORT)
   endif()
-  cet_make_library(LIBRARY_NAME "${plugin_base}_${SUFFIX}${thunk}"
-    ${LIB_TYPE}
-    ${BP_PLUGIN_SOURCE}
+  cet_make_library(LIBRARY_NAME "${plugin_stem}_${SUFFIX}${target_thunk}"
+    ${REG_LIB_TYPE}
+    ${BP_REG_SOURCE}
     ${cml_common_args} ${cml_impl_args}
     LIBRARIES ${BP_LIBRARIES}
   )
-  if (thunk)
-    set_target_properties(${plugin_base}_${SUFFIX}${thunk}
-      PROPERTIES OUTPUT_NAME "${plugin_base}_${SUFFIX}"
+  if (target_thunk)
+    set_target_properties(${plugin_stem}_${SUFFIX}${target_thunk}
+      PROPERTIES OUTPUT_NAME "${plugin_stem}_${SUFFIX}"
     )
   endif()
 endfunction()
