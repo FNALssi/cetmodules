187ccaafb65b5d862faf927d428863fe41d54cc4
Chris Green <greenc@fnal.gov>
2021-03-05 15:39:07 -0600

    Better warning.

--- Modules/BasicPlugin.cmake
+++ Modules/BasicPlugin.cmake
@@ -158,7 +158,8 @@ function(basic_plugin NAME SUFFIX)
   cmake_parse_arguments(PARSE_ARGV 2 BP
     "${cet_bp_flags}" "${cet_bp_one_arg_opts}" "${cet_bp_list_options}")
   if (BP_UNPARSED_ARGUMENTS)
-    warn_deprecated("unprocessed arguments" NEW "LIBRARIES")
+    warn_deprecated("use of non-option arguments (${BP_UNPARSED_ARGUMENTS})"
+      NEW "LIBRARIES")
     list(APPEND BP_LIBRARIES ${BP_UNPARSED_ARGUMENTS})
   endif()
   if (BP_BASENAME_ONLY AND BP_USE_PRODUCT_NAME)
