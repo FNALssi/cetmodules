8bce209827c53e22fffb8f607eac8597a2c9d291
Chris Green <greenc@fnal.gov>
2022-03-22 19:29:50 -0500

    Reorganize for multi-version

--- _sources/module/compat/find/FindRange-v3.rst.txt
+++
@@ -1,1 +0,0 @@
-.. cmake-module:: /../Modules/compat/FindRange-v3.cmake
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'git-develop-189750f',
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
