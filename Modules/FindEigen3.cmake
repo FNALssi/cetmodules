db2498e8d5c1185a8f1c9e06ed230c94cc1a4af0
Chris Green <greenc@fnal.gov>
2022-04-25 11:29:01 -0500

    Simplify, and match target name provided by Eigen >=3.4.0

--- Modules/FindEigen3.cmake
+++ Modules/FindEigen3.cmake
@@ -1,22 +1,13 @@
 if (NOT Eigen3_FOUND)
-  set(_cet_findEigen3_quietly ${Eigen3_FIND_QUIETLY})
-  find_package(Eigen3 CONFIG QUIET)
-  set(Eigen3_FIND_QUIETLY ${_cet_findEigen3_quietly})
-  unset(_cet_findEigen3_quietly)
+  find_package(Eigen3 CONFIG)
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Eigen3
-  CONFIG_MODE
-  HANDLE_VERSION_RANGE
-  REQUIRED_VARS EIGEN3_INCLUDE_DIRS
-)
-
 if (Eigen3_FOUND
-    AND NOT TARGET Eigen3::Eigen3
+    AND NOT TARGET Eigen3::Eigen
     AND EIGEN3_INCLUDE_DIRS)
-  add_library(Eigen3::Eigen3 INTERFACE IMPORTED)
-  set_target_properties(Eigen3::Eigen3 PROPERTIES
+  # < Eigen 3.4.0
+  add_library(Eigen3::Eigen INTERFACE IMPORTED)
+  set_target_properties(Eigen3::Eigen PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIRS}"
     INTERFACE_COMPILE_DEFINITIONS "${EIGEN3_DEFINITIONS}"
   )
