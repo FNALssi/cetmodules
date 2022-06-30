1566a849a112e36c6cad493a5b8082c1dcc7cdc3
Art CI system <ci_build_reports@fnal.gov>
2022-06-29 23:24:59 -0500

    Cetmodules documentation update (develop-e94faba)

--- git-develop/_static/documentation_options.js
+++ git-develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'git-develop-c0e2197',
+    VERSION: 'git-develop-e94faba',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
