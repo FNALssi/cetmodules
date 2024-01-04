d4f56803ecc2521ddc1860dfb777ab75c86ad727
Chris Green <greenc@fnal.gov>
2024-01-04 12:13:05 -0600

    Consolidate handling of Cetmodules' minimum required CMake version
    * New exported project variable `cetmodules_MIN_CMAKE_VERSION`.
    
    * `CETMODULES_MIN_CMAKE_VERSION` -> `cetmodules_MIN_CMAKE_VERSION`.
    
    * Generate rather than copy `etc/cmake_required_version.txt` for single
      point of maintenance.
    
    * Use @cetmodules_MIN_CMAKE_VERSION@ instead of
      @CMAKE_MINIMUM_REQUIRED_VERSION@ in generated `COMPILE_ONLY`
      `CMakeLists.txt` files to avoid the consequences pollution of
      @CMAKE_MINIMUM_REQUIRED_VERSION@ by user-included CMake files.

--- config/cetmodules-cmake-version-check.cmake
+++ config/cetmodules-cmake-version-check.cmake
@@ -1,3 +1,3 @@
-if (CMAKE_VERSION VERSION_LESS @CETMODULES_MIN_CMAKE_VERSION@)
-  message(FATAL_ERROR "several features of cetmodules require CMake>=@CETMODULES_MIN_CMAKE_VERSION@")
+if (CMAKE_VERSION VERSION_LESS @cetmodules_MIN_CMAKE_VERSION@)
+  message(FATAL_ERROR "several features of cetmodules require CMake>=@cetmodules_MIN_CMAKE_VERSION@")
 endif()
