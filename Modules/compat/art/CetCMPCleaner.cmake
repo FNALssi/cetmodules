2e4b5e839926566f95ee76ce8a194b2f871d621d
Chris Green <greenc@fnal.gov>
2025-01-27 17:05:22 -0600

    Avoid duplication of `OPTIONAL`

--- Modules/compat/art/CetCMPCleaner.cmake
+++ Modules/compat/art/CetCMPCleaner.cmake
@@ -2,7 +2,7 @@
 X
 -
 #]================================================================]
-include_guard()
+include_guard(GLOBAL)
 
 cmake_minimum_required(VERSION 3.20...3.27 FATAL_ERROR)
 
@@ -10,8 +10,6 @@ set(_cet_cmp_compat_dir "${CMAKE_CURRENT_LIST_DIR}" CACHE INTERNAL "art suite co
 set(_cet_cmp_compat_art_version 3.14.05 CACHE INTERNAL "art suite compatibility modules version")
 set(_cet_cmp_compat_canvas_root_io_version 1.11.00-alpha CACHE INTERNAL "canvas_root_io compatibility modules version")
 set(_cet_cmp_compat_messagefacility_version 2.10.00-alpha CACHE INTERNAL "messagefacility compatibility modules version")
-
-include_guard(GLOBAL)
 
 if (COMMAND _include)
   message(WARNING "include() has already been overridden: compatibility with art suites < ${_cet_cmp_compat_art_version} cannot be assured")
@@ -63,7 +61,8 @@ macro(include _cmp_FILE)
   endif()
   # Have we been asked to make this module optional, overriding call
   # options?
-  if (CETMODULES_OPTIONAL_INCLUDE_MODULE_${_cmp_module})
+  if (CETMODULES_OPTIONAL_INCLUDE_MODULE_${_cmp_module} AND
+      NOT "${ARGV}" MATCHES "(^|[; 	])OPTIONAL([; 	]|$)")
     unset(_cmp_module)
     _include(${ARGV} OPTIONAL)
   else()
